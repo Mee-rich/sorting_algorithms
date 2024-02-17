ddc60589adbf428c91b75412376ecd304c692ae1
mee-rich <jamessuyi@gmail.com>
2024-02-17 06:48:44 +0100

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Shell sort - Knuth Sequence
+ * @array: array to sort
+ * @size: size of array
+ */
+void shell_sort(int *array, size_t size)
+{
+	/* knuth sequence n+1 = n * 3 + 1 */
+
+	int gap = 1, i, j, temp;
+
+	while (gap < ((int)(size)))
+		gap = (3 * gap) + 1;
+
+	for (gap = (gap - 1) / 3; gap > 0; gap = (gap - 1) / 3)
+	{
+		for (i = gap; i < (int)size; i += 1)
+		{
+			temp = array[i];
+			
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+				array[j] = array[j - gap];
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
+}
