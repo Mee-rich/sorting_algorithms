da435863d5265c7fc7f2498b8dd5684e8f0db1af
mee-rich <jamessuyi@gmail.com>
2024-02-17 01:32:16 +0100

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,71 @@
+#include "sort.h"
+
+/**
+ * partition - Perform partitioning for Quick Sort
+ * @array: Array to be sorted
+ * @low: Starting index
+ * @high: Ending index
+ * @size: Size of the array
+ *
+ * Return: Index of the pivot element
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+
+	for (int j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				int temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+
+	if (array[i + 1] != array[high])
+	{
+		int temp = array[i + 1];
+		array[i + 1] = array[high];
+		array[high] = temp;
+		print_array(array, size);
+	}
+	return (i + 1);
+}
+
+/**
+ * quicksort - Implement Quick Sort recursively
+ * @array: Array to be sorted
+ * @low: Starting index
+ * @high: Ending index
+ * @size: Size of the array
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pivot = partition(array, low, high, size);
+
+		quicksort(array, low, pivot - 1, size);
+		quicksort(array, pivot + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using Quick Sort
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	quicksort(array, 0, size - 1, size);
+}
+
